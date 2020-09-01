081b1671ccf1a14cabd55cf6fa6bb1b86ac1112b
Kyriakos Kaperonis <Kyriakos.Kaperonis@citrix.com>
2020-09-02 01:28:15 +0300

    add Navigation Bar

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -1,11 +1,24 @@
 import React from "react";
+import  Navbar from "react-bootstrap/NavBar";
+import Nav from "react-bootstrap/Nav";
+
 function Header(props) {
+  const isLoggedIn = true;
   return (
-    <nav className="navbar navbar-dark bg-primary">
-      <div className="row col-12 d-flex justify-content-center text-white">
-        <span className="h3">{props.title}</span>
-      </div>
-    </nav>
+    <>
+      {isLoggedIn ? (
+        <Navbar bg="dark" variant="dark">
+          <Nav className="mr-auto">
+            <Nav.Link href="register">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Navbar>
+      ) : (
+        <Navbar bg="dark" variant="dark">    
+      </Navbar>
+      )}
+    </>
   );
 }
 export default Header;
